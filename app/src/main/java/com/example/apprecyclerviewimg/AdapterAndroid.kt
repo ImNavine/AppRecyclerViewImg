ec717cfec2af0c0f206a74d8bbdeaddb48461ab3
ImNavine <kevinadine14@gmail.com>
2024-02-26 14:34:29 -0500

    Ejercicio Terminado

--- app/src/main/java/com/example/apprecyclerviewimg/AdapterAndroid.kt
+++ app/src/main/java/com/example/apprecyclerviewimg/AdapterAndroid.kt
@@ -1,5 +1,34 @@
 package com.example.apprecyclerviewimg
 
-class AdapterAndroid {
-    
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.apprecyclerviewimg.databinding.ItemAndroidBinding
+
+class AdapterAndroid (val listaAndroid: List<Android>, val context:Context)
+    : RecyclerView.Adapter<AdapterAndroid.ViewHolder>(){
+    inner class ViewHolder(val binding: ItemAndroidBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding=ItemAndroidBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun getItemCount()=listaAndroid.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder){
+            with(listaAndroid[position]){
+                binding.tvnomversion.text=nombre
+                binding.tvnumversion.text=version
+                binding.ivandroid.setImageResource(img)
+                binding.cvandroid.setOnClickListener(View.OnClickListener {
+                    Toast.makeText(context, "Click Item Android $nombre", Toast.LENGTH_LONG).show()
+                })
+            }
+        }
+    }
 }
